5d48d5e84621c2eeadf7f73e5a99326ae4c640ba
Al Zaplin <zaplin.dev@gmail.com>
2025-02-10 21:36:08 +0300

    add(code & notes) For lesson #3 "Nested Components in Vue"

---
+++ comments/3/App copy.vue
@@ -0,0 +1,17 @@
+<script setup>
+import PlanPicker from './components/PlanPicker.vue'
+// * 2.0 Если мы хотим использовать комп. "CoffeePlan" ещё где-то, то нам придётся снова копировать массив с названиями планов, давайте сделаем этот момент чуть более универсальным добавив ещё один комп. для этого "PlanPicker".
+// Go to [src\components\PlanPicker.vue]
+</script>
+
+<template>
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+    <h2 class="subtitle">
+      We travel the world to source the very best single origin coffee for you
+    </h2>
+    <!-- 2.2 Заменим CoffeePlan на PlanPicker и всё работает как раньше. -->
+    <!-- Go to [src\components\PlanPicker.vue] -->
+    <PlanPicker />
+  </div>
+</template>
